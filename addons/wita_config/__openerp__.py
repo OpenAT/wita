3b9283aec704627cae124f393937af7157cad520
Michael Karrer <michaelkarrer81@gmail.com>
2017-12-06 17:25:08 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/wita_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'wita_config',
+    'summary': '''FS-Online wita instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance wita
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
