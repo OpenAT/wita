885e6e55946cafe14b6fd08c7ca8a79313ecfb32
Michael Karrer <michaelkarrer81@gmail.com>
2020-05-20 11:45:10 +0200

    [REM] wita_hide_checkout_billing_fields.js was uncommented

--- addons/wita_config/static/src/js/wita_hide_checkout_billing_fields.js
+++ addons/wita_config/static/src/js/wita_hide_checkout_billing_fields.js
@@ -1,70 +1,70 @@
-$(document).ready(function () {
-
-    // Init
-    var billing_input_fields_to_hide = {
-        'birthdate': 'input',
-        'phone': 'input',
-        'street': 'input',
-        'street_number_web': 'input',
-        'zip': 'input',
-        'city': 'input',
-        'country_id': 'select',
-    };
-    var billing_input_fields_to_hide_mandatory = [
-        'birthdate',
-        'street',
-        'zip',
-        'city',
-    ];
-    for (var cif in billing_input_fields_to_hide) {
-        billing_input_fields_to_hide[cif] = $(".billing-fields " + billing_input_fields_to_hide[cif] + "[name='" + cif + "']");
-    }
-    var $donation_deduction_optout_web = $(".billing-fields input[name='donation_deduction_optout_web']");
-
-
-    function wita_hide_billing(fields, mandatory_fields, $checkbox) {
-
-        if ($checkbox.is(":checked")) {
-            // Show the fields
-            for (var f_name in fields) {
-
-                // enable mandatory for some fields
-                if (mandatory_fields.includes(f_name)) {
-                    fields[f_name].attr('required', 'True');
-                    fields[f_name].attr('aria-required', 'true');
-                    var $f_label = $(".billing-fields label[for='" + f_name + "']");
-                    $f_label.addClass('mandatory text-danger');
-                }
-
-                // show form-roup
-                fields[f_name].closest('.form-group').show();
-            }
-
-        } else {
-            // Hide the fields
-            for (var f_name in fields) {
-
-                // disable mandatory
-                if (mandatory_fields.includes(f_name)) {
-                    fields[f_name].removeAttr('required');
-                    fields[f_name].removeAttr('aria-required');
-                    var $f_label = $(".billing-fields label[for='" + f_name + "']");
-                    $f_label.removeClass('mandatory text-danger');
-                }
-
-                // hide form-group
-                fields[f_name].closest('.form-group').hide();
-            }
-
-        }
-    }
-
-    // First run
-    wita_hide_billing(billing_input_fields_to_hide, billing_input_fields_to_hide_mandatory, $donation_deduction_optout_web);
-
-    // Run on onchange of the checkbox
-    $donation_deduction_optout_web.on("change", function (ev) {
-        wita_hide_billing(billing_input_fields_to_hide, billing_input_fields_to_hide_mandatory, $donation_deduction_optout_web);
-    });
-
-});
+// $(document).ready(function () {
+//
+//     // Init
+//     var billing_input_fields_to_hide = {
+//         'birthdate': 'input',
+//         'phone': 'input',
+//         'street': 'input',
+//         'street_number_web': 'input',
+//         'zip': 'input',
+//         'city': 'input',
+//         'country_id': 'select',
+//     };
+//     var billing_input_fields_to_hide_mandatory = [
+//         'birthdate',
+//         'street',
+//         'zip',
+//         'city',
+//     ];
+//     for (var cif in billing_input_fields_to_hide) {
+//         billing_input_fields_to_hide[cif] = $(".billing-fields " + billing_input_fields_to_hide[cif] + "[name='" + cif + "']");
+//     }
+//     var $donation_deduction_optout_web = $(".billing-fields input[name='donation_deduction_optout_web']");
+//
+//
+//     function wita_hide_billing(fields, mandatory_fields, $checkbox) {
+//
+//         if ($checkbox.is(":checked")) {
+//             // Show the fields
+//             for (var f_name in fields) {
+//
+//                 // enable mandatory for some fields
+//                 if (mandatory_fields.includes(f_name)) {
+//                     fields[f_name].attr('required', 'True');
+//                     fields[f_name].attr('aria-required', 'true');
+//                     var $f_label = $(".billing-fields label[for='" + f_name + "']");
+//                     $f_label.addClass('mandatory text-danger');
+//                 }
+//
+//                 // show form-roup
+//                 fields[f_name].closest('.form-group').show();
+//             }
+//
+//         } else {
+//             // Hide the fields
+//             for (var f_name in fields) {
+//
+//                 // disable mandatory
+//                 if (mandatory_fields.includes(f_name)) {
+//                     fields[f_name].removeAttr('required');
+//                     fields[f_name].removeAttr('aria-required');
+//                     var $f_label = $(".billing-fields label[for='" + f_name + "']");
+//                     $f_label.removeClass('mandatory text-danger');
+//                 }
+//
+//                 // hide form-group
+//                 fields[f_name].closest('.form-group').hide();
+//             }
+//
+//         }
+//     }
+//
+//     // First run
+//     wita_hide_billing(billing_input_fields_to_hide, billing_input_fields_to_hide_mandatory, $donation_deduction_optout_web);
+//
+//     // Run on onchange of the checkbox
+//     $donation_deduction_optout_web.on("change", function (ev) {
+//         wita_hide_billing(billing_input_fields_to_hide, billing_input_fields_to_hide_mandatory, $donation_deduction_optout_web);
+//     });
+//
+// });
