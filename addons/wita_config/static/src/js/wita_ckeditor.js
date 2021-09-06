67f5cb12d27f35135a21cb1d3ed2ab11fd3ee580
Joachim Grubelnik <j.grubelnik@openat.at>
2021-09-06 15:11:33 +0200

    [ADD] change css styles, add snippet options, add news ckedit options

--- addons/wita_config/static/src/js/wita_ckeditor.js
+++ addons/wita_config/static/src/js/wita_ckeditor.js
@@ -40,7 +40,7 @@
             ];
 
             // Add Custom font and font background colors
-            config.colorButton_colors = 'ffffff,333333,666666,F19130,8c8c8c,ea470d';
+            config.colorButton_colors = 'ffffff,333333,666666,97a7b0,72848d,394b53,f7a600,ef7c00,e74406,c22b02,9b0600';
             // config.colorButton_colorsPerRow = 3;
             config.colorButton_enableAutomatic = true;
 
@@ -54,6 +54,12 @@
                 {name: 'H4', element: 'h4'},
                 {name: 'H5', element: 'h5'},
                 {name: 'H6', element: 'h6'},
+                {name: 'H1-OS', element: 'h1' , attributes : { 'class' : 'h1-os' } },
+                {name: 'H2-OS', element: 'h2' , attributes : { 'class' : 'h2-os' } },
+                {name: 'H3-OS', element: 'h3' , attributes : { 'class' : 'h3-os' } },
+                {name: 'H4-OS', element: 'h4' , attributes : { 'class' : 'h4-os' } },
+                {name: 'H5-OS', element: 'h5' , attributes : { 'class' : 'h5-os' } },
+                {name: 'H6-OS', element: 'h6' , attributes : { 'class' : 'h6-os' } },
                 {name: 'Formatted', element: 'pre'},
                 {name: 'Address', element: 'address'},
             ];
