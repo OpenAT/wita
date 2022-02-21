f01937055e34089796b4f15754121ece2b112749
Joachim Grubelnik <j.grubelnik@openat.at>
2022-02-21 17:02:50 +0100

    [ADD] adding new module for donor journey

---
+++ addons/website_sale_ekw/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+{
+    'name': 'website_sale_ekw',
+    'summary': """website_sale_ekw""",
+    'description': """
+grid for the /shop pages
+    """,
+    'author': 'Joachim Grubelnik (joachim.grueblnik@datadialog.net), DataDialog',
+    'version': '1.1',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'website',
+        'website_sale',
+        'website_sale_donate',
+        'website_sale_categories',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+}
---
+++ addons/website_sale_ekw/controllers/main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.website_sale.controllers import main
+
+main.PPG = 9
+main.PPR = 3
